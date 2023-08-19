b616907266abf61d31c408a4a0fea6a24d13ab03
Kushal Agrawal <agrawalskushal@gmail.com>
2023-08-19 18:39:00 +0530

    Selling your stock and other options

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ server.post("/buystock", (req, res) => {
   let id = uuidv4();
   if (type == "virtual") {
     balanceObj = data["virtualwallet"].find((item) => item.id === email);
+    
     balance = balanceObj.balance;
   } else {
     balanceObj = data["realwallet"].find((item) => item.id === email);
@@ -26,7 +27,23 @@ server.post("/buystock", (req, res) => {
     res.status(401).json({ success: false, error: "Not Enough Money" });
   } else {
     balanceObj.balance -= buyprice * count;
-
+    balanceObj.invested += buyprice * count;
+    balanceObj.recentstock = {
+        symbol: symbol,
+        buyprice: buyprice,
+        count: count,
+        total: buyprice * count
+    };
+    if(balanceObj.recentfive.length == 5){
+        balanceObj.recentfive.splice(0, 1);
+    }
+    balanceObj.recentfive.push({
+        symbol: symbol,
+        buyprice: buyprice,
+        id: id,
+        count: count,
+        total: buyprice * count
+    });
     if (type == "virtual") {
       data["vholdings"].push({
         id: id,
@@ -48,19 +65,19 @@ server.post("/buystock", (req, res) => {
         kind: kind,
       });
     }
-
-    data["transaction"].push({
-      id: id,
-      kind: kind,
-      symbol: symbol,
-      name: name,
-      count: count,
-      buyprice: buyprice,
-      email: email,
-      event: "userbuying",
-    });
+    let transactionData = {
+        id: id,
+        kind: kind,
+        symbol: symbol,
+        name: name,
+        count: count,
+        buyprice: buyprice,
+        email: email,
+        event: "userbuying",
+      };
+    data["transaction"].push(transactionData);
     fs.writeFileSync("db.json", JSON.stringify(data, null, 2));
-    res.status(200).json({ success: true, error: null });
+    res.status(200).json({ success: true, error: null, data: transactionData });
   }
 });
 
@@ -82,30 +99,32 @@ server.post("/squareoff", (req, res) => {
 
   if (type == "virtual") {
     balanceObj = data["virtualwallet"].find((item) => item.id === email);
-    balanceObj.balance += (holdingObj.kind == "buy" ? sellPrice - holdingObj.buyprice : holdingObj.buyprice -sellPrice);
+    balanceObj.balance += (holdingObj.kind == "buy" ? sellPrice*holdingObj.count : (2*holdingObj.buyprice -sellPrice)*holdingObj.count);
+    balanceObj.invested -= holdingObj.buyprice * holdingObj.count;
     if (index > -1) {
       data["vholdings"].splice(index, 1);
     }
   } else {
     balanceObj = data["realwallet"].find((item) => item.id === email);
-    balanceObj.balance += (holdingObj.kind == "buy" ? sellPrice - holdingObj.buyprice : holdingObj.buyprice -sellPrice);
+    balanceObj.balance += (holdingObj.kind == "buy" ? sellPrice*holdingObj.count : (2*holdingObj.buyprice -sellPrice)*holdingObj.count);
+    balanceObj.invested -= holdingObj.buyprice * count;
     if (index > -1) {
       data["rholdings"].splice(index, 1);
     }
   }
-
-  data["transaction"].push({
+  let transactionData = {
     id: uuidv4(),
     kind: holdingObj.kind,
     symbol: holdingObj.symbol,
     name: holdingObj.name,
     count: holdingObj.count,
-    buyprice: holdingObj.buyprice,
+    sellprice: sellPrice,
     email: holdingObj.email,
     event: "userselling",
-  });
+  };
+  data["transaction"].push(transactionData);
   fs.writeFileSync("db.json", JSON.stringify(data, null, 2));
-  res.status(200).json({ success: true, error: null });
+  res.status(200).json({ success: true, error: null, data: transactionData });
 });
 server.use(router);
 
