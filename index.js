ba4443c39dc514b8c5cfc92c6f713eb452c86a4a
Kushal Agrawal <agrawalskushal@gmail.com>
2023-08-20 13:52:48 +0530

    chat functionality added

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const { v4: uuidv4 } = require("uuid");
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+const tradingChatResponses = {
+  "hi": "Hello, My name is GarnerBot. How can I help you?",
+  "how do i start trading stocks": "To start trading stocks, you'll need to open a brokerage account, research companies you want to invest in, and place buy or sell orders through your broker. It's important to learn about stock markets and strategies first.",
+  "is it safe for teens to trade stocks": "Yes, teens can trade stocks, but it's important to do it responsibly. Consider using a custodial account with parental supervision. Start with a small amount of money and focus on learning before investing heavily.",
+  "what is the stock market": "The stock market is a place where people buy and sell shares of publicly traded companies. It allows companies to raise capital by selling ownership stakes, and investors can profit from price fluctuations.",
+  "how do i research stocks": "You can research stocks by studying a company's financial reports, analyzing its industry, and keeping an eye on news and trends. Many websites and apps provide stock market research tools for free.",
+  "what are the risks of trading stocks": "Stock trading carries risks, including the potential loss of money. Stock prices can be volatile, and it's possible to lose more than you invest. Diversification and risk management are essential.",
+  "what trading strategies should i consider": "Common trading strategies include day trading, swing trading, and long-term investing. Each has its own risk and reward profile. Start with a strategy that matches your risk tolerance and time commitment.",
+  "are there any good books or courses on trading": "Yes, there are many books and online courses that can help you learn about trading. Some popular options include 'The Intelligent Investor' by Benjamin Graham and online courses from platforms like Coursera and Udemy.",
+  "can you recommend any trading apps for teens": "Certainly! Some trading apps suitable for teens include Robinhood, E*TRADE, and Webull. Make sure to check the age requirements and parental controls on these platforms.",
+  "what is short selling": "Short selling is a trading strategy where an investor borrows a stock and sells it with the hope that its price will decrease. They then buy the stock back at a lower price to return it to the lender, making a profit from the price difference.",
+  "tell me more about garner (the trading app)": "Garner is a stock trading app developed by Barings bank. It offers a user-friendly platform for trading stocks, accessing research and market data, and managing your investments. It's a great choice for both beginners and experienced traders.",
+  "how can i protect my investments from market crashes": "To protect your investments from market crashes, consider diversifying your portfolio by investing in different asset classes, such as stocks, bonds, and real estate. Additionally, having an emergency fund and a long-term investment horizon can help you weather market downturns.",
+};
 server.post("/buystock", (req, res) => {
   console.log(req.body);
   try {
@@ -219,10 +233,25 @@ server.patch("/setparent", (req,res) => {
     fs.writeFileSync("db.json", JSON.stringify(data, null, 2));
     res.status(200).json({ success: true, error: null, data: "Modified Successfully" });
   } catch (error) {
-    res.status(500).json({ success: false, error: "Internal Server Error" });
+    res.status(500).json({ success: false, error: error });
+    
+  }
+});
+server.post("/chat", (req, res) => {
+  let {chatrequest: chatrequest} = req.body;
+  try {
+    chatrequest = chatrequest.replace(/\?/g, '').toLowerCase();
+    if(Object.keys(tradingChatResponses).find((item) => item === chatrequest) === undefined){
+      res.status(200).json({success: false, error: "No Request Found", data: "I am still learning. Meanwhile you can refer web for your questions."});
+    }
+    else{
+      res.status(200).json({success: true, error: null, data: tradingChatResponses[chatrequest]});
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: error });
     console.log(error);
   }
-})
+});
 server.use(router);
 
 server.listen(3000, () => {
